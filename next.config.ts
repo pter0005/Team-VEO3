ba7b115f4bc20b0e89567d2afa30f790ecbb6ae8
pedro henrique <dnlsniperonly2@gmail.com>
2025-06-06 01:49:50 +0000

    no card pov seu time perdeu coloque https://www.youtube.com/watch?v=VTxW

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.imgur.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
